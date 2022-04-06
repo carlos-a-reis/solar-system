9f1abffcfdd387293c66e86332104e718f60bdfd
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-04-06 17:35:08 -0300

    Adiciona decimo requisito do projeto

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import MissionsCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
+    const missionList = missions.map((mission, index) => {
+      const { name, year, country, destination } = mission;
+      return (<MissionsCard
+        name={ name }
+        year={ year }
+        country={ country }
+        destination={ destination }
+        key={ index }
+      />);
+    });
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <div id="missons">
+          {missionList}
+        </div>
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Title from './Title';
 import PlanetCard from './PlanetCard';
-import Planets from '../data/planets';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
-    const planetList = Planets.map((planet, index) => {
+    const planetList = planets.map((planet, index) => {
       const { name, image } = planet;
       return <PlanetCard planetName={ name } planetImage={ image } key={ index } />;
     });
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <ul>
+        <div id="planets">
           {planetList}
-        </ul>
+        </div>
       </div>
     );
   }
