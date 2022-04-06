3d1e2ff7022a125e65608c6e0565fb1f3ea625b7
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-04-06 17:11:33 -0300

    Adiciona o setimo requisito do projeto

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <h2> </h2>
+      </div>
+    );
+  }
+}
+
+export default Missions;
