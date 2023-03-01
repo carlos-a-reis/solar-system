3313c450d28b6a66151efe3781440f68f9f100db
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-03-01 20:49:29 -0300

    refactor: corrige a importação do componente css Main

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 import Background from './assets/images/background.png';
 
-export default Main = styled.main`
+const Main = styled.main`
   background-image: url(${Background});
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   color: white;
 `;
+
+export default Main;
