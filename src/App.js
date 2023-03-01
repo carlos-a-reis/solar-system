709d75572ad3e2bd45bb98c95af5bfdf6de91466
Carlos Augusto dos Reis <hadqen@gmail.com>
2023-03-01 20:02:58 -0300

    feat: adiciona ambiente para criação do css com styled components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
-import './App.css';
+import Main from './styles';
 
 class App extends React.Component {
   render() {
     return (
-      <main>
+      <Main>
         <Header />
         <SolarSystem />
         <Missions />
-      </main>
+      </Main>
     );
   }
 }
